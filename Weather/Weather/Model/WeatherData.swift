12c48a3befe0154345fca0966a3915926fb05421
Sahil Saxena <122679806+saxenasahil873@users.noreply.github.com>
2023-05-14 02:55:28 -0700

    Add files via upload

---
+++ Weather/Weather/Model/WeatherData.swift
@@ -0,0 +1,23 @@
+//
+//  WeatherData.swift
+//  Weather
+//
+//  Created by Sahil Saxena on 03/02/2023.
+//
+
+import Foundation
+
+struct WeatherData: Codable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Codable {
+    let temp: Double
+}
+
+struct Weather: Codable {
+    let description: String
+    let id: Int
+}
